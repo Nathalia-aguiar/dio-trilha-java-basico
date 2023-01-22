2824e6cb1e22f815c5e77c632eb67c91858e19ac
Nathália Souza <nathyaguiarsouza@gmail.com>
2023-01-22 19:44:59 -0300

    add anotações de operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,30 +1,21 @@
 public class Operadores {
+
      public static void main(String[] args) {
-        String nomeCompleto = "LINGUAGEM" + "JAVA";
+        //OPERADOR DE INCREMENTO
+      int numero = 5;
+      numero = - numero;
+      System.out.println(numero);
+      numero = + numero;
+      numero = numero * -1;
+      System.out.println(numero);
 
+      //OPERADOR TERNARIO 
+      
+      int a = 1;
+      int b = 1;
+      String resultado = a==b ? "Verdadeiro" : "False";
 
-        //qual o resultado das expressoes abaixo?
-        String concatenacao ="?"; 
+      System.out.println(resultado);
 
-        concatenacao = 1+1+1+"1";
-
-        System.out.println(concatenacao);
-
-        concatenacao = 1+"1"+1+1;
-
-        System.out.println(concatenacao);
-
-        concatenacao = 1+"1"+1+"1";
-
-        System.out.println(concatenacao);
-
-        concatenacao = "1"+1+1+1;
-
-        System.out.println(concatenacao);
-
-        concatenacao = "1"+(1+1+1);
-
-        System.out.println(concatenacao);
-        
     }
 }
