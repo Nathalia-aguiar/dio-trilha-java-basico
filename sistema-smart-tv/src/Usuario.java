c0f1356b5f09102b10839cf54f19f8c067a42fef
Nathália Souza <nathyaguiarsouza@gmail.com>
2023-01-22 20:24:58 -0300

    novo projeto

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,29 @@
+public class Usuario {
+
+    public static void main(String[] args) {
+        
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        System.out.println("Canal atual " + smartTv.canal);
+        smartTv.mudarCanal(13);
+        System.out.println("Canal atual " + smartTv.canal);
+
+
+        System.out.println("A TV está ligada? " + smartTv.ligada);
+        System.out.println("O canal atual: " +smartTv.canal);
+        System.out.println("Volume Atual: " +smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo status -> TV está ligada? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo status -> TV está ligada? " + smartTv.ligada);
+
+    }
+}
