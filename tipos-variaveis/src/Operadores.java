d73eb4ee454c12896cb4169e51f817ca40715908
Nathália Souza <nathyaguiarsouza@gmail.com>
2023-01-22 13:39:10 -0300

    exercício da aula 2 de operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,3 +1,30 @@
 public class Operadores {
-    
+     public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+
+        //qual o resultado das expressoes abaixo?
+        String concatenacao ="?"; 
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);
+        
+    }
 }
